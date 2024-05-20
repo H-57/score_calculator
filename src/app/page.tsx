8fea093c75531f1a12981350b411ba3c7abdf806
tarun <tarunkumar8059@gmail.com>
2024-05-20 15:46:33 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,120 @@
-"use client"
+"use client";
 
+import Navbar from "@/components/NavbarComponent";
 import axios from "axios";
-import { useState } from "react";
-
-
+import { ChangeEvent, useState } from "react";
+import { toast } from "react-toastify";
 
 export default function Home() {
-  const [Url, setUrl] = useState("")
+  const [form, setForm] = useState({
+    url: "",
+    markScheme: null,
+    negative: null,
+  });
+  const [data, setData] = useState<any>();
+
+  const handelForm = (e: any) => {
+    e.preventDefault();
+    if(form.url==""||!form.markScheme||!form.negative){
+    return  toast.error("Please fill all the fields")
+    }
+      
+    axios
+      .post("/api/pdf", form)
+      .then(function (response) {
+        console.log(response.data);
+        setData(response.data.Result);
+      })
+      .catch(function (error) {
+        console.log(error);
+        toast.error(error.message)
+      });
+  };
+
   return (
-   
-<>
-<form className="max-w-sm mx-auto" >
-  <div className="mb-5">
-    <label htmlFor="url" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-    <input value={Url} onChange={(e) => setUrl(e.target.value)} type="text" id="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="enter your answerkey url" required />
-  </div>
+    <>
+      <Navbar />
+      <form className="max-w-sm mx-auto pt-20 px-5 border rounded-md pb-10 ">
+        <div className="mb-5">
+          <label
+            htmlFor="url"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your answer key url
+          </label>
+          <input
+            value={form.url}
+            onChange={(e) =>
+              setForm({ ...form, [e.target.name]: e.target.value })
+            }
+            name="url"
+            type="text"
+            id="url"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="enter your answerkey url"
+            required
+          />
+        </div>
 
+        <select
+          name="markScheme"
+          id="marks"
+          className=" w-full bg-gray-200 p-1 rounded-sm my-5"
+          required
+          onChange={(e) =>
+            setForm({ ...form, [e.target.name]: e.target.value })
+          }
+        >
+          <option value={""}>Chosses Marks Scheme</option>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+        </select>
+       
+        <select
+          onChange={(e) =>
+            setForm({ ...form, [e.target.name]: e.target.value })
+          }
+          name="negative"
+          id="negative"
+          className=" w-full bg-gray-200 p-1 rounded-sm my-5"
+          required
+        >
+          <option value={""}>Chosses negative Marks Scheme</option>
+          <option value={0}>0</option>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+        </select>
 
-  <button onClick={(e) =>{ e.preventDefault();axios.post('/api/pdf',{url:Url}).then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });}} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-</form>
+        <button
+          onClick={handelForm}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
 
-</>
+    {data&&  <div className="flex  mt-10 text-xl text-white px-10 gap-32 flex-col md:flex-row">
+    <section className="space-y-4">
+      <p>Positive Marks :{data?.right*form?.markScheme!}</p>
+      <p>Negative Marks :{data?.wrong*form?.negative!}</p>
+      <hr />
+      <p>Total Marks:{(data?.right*form.markScheme!)-data?.wrong*form.negative!}</p>
+    </section>
+    <section className="space-y-4">
+      <p>Attempted :{data.total-data.unattempt}</p>
+      <p>unAttempted :{data.unattempt}</p>
+      <p>Correct Attempt :{data.right}</p>
+      <p>Incorrect Attempt :{data.wrong}</p>
+      <hr />
+      <p>Accuracy :{(data.right/(data.total-data.unattempt)*100).toFixed(2)}</p>
+      
+
+    </section>
+      </div>}
+    </>
   );
 }
---
+++ src/components/NavbarComponent.tsx
@@ -0,0 +1,122 @@
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Bars3BottomLeftIcon, XMarkIcon,HomeIcon,UserIcon,BookmarkIcon,ArrowDownIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+import {motion} from 'framer-motion';
+
+const Navbar = () => {
+
+  const [activeItem, setActiveItem] = useState<string>("Home");
+  const [nav, setNav] = useState<String>("");
+  const [scrollY, setscrollY] = useState<number>(10);
+  const handleScroll = () => {
+    if (window.scrollY > 10) {
+      if (window.scrollY > scrollY) {
+        setNav("hidden");
+        // console.log("hide");
+      } else {
+        setNav("flex");
+        // console.log("show");
+      }
+    } else {
+      setNav("");
+    }
+
+    setscrollY(window.scrollY);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrollY]);
+
+  const links = [
+    { name: "Home", url: "/" ,icon:<HomeIcon className="w-6 h-6" />},
+    // { name: "", url: "/projects",icon:<BookmarkIcon className="w-6 h-6" /> },
+    { name: "AboutUs", url: "/about",icon:<UserIcon className="w-6 h-6" /> },
+  ];
+  const [toggel, setToggel] = useState<boolean>(false);
+
+  return (
+    <>
+      <nav
+        className={` filter  fixed z-20 flex justify-between   text-white w-screen transition-all ease-in-out duration-1000 transform ${nav} ${
+          scrollY > 20 ? "bg-[#050816]" : "bg-[#050816]"
+        }`}
+      >
+        <div className="px-5 xl:px-12 py-6  w-full block float-right ">
+          <Link className="text-3xl font-bold font-heading " href="/">
+            <Image
+              className="w-[4rem] h-auto absolute"
+              height={400}
+              width={40}
+              src="/logo.png"
+              alt="logo"
+            />
+          </Link>
+          {/* Nav Links */}
+          <ul
+            className={`hidden float-right md:flex px-4 mx-auto font-semibold font-heading space-x-12 `}
+          >
+            {links.map((link) => (
+              <li key={link.name}>
+                <Link
+                onClick={()=>{setActiveItem(link.name)}}
+                  className={`hover:text-violet-800 capitalize ${activeItem===link.name?"text-violet-800":""}`}
+                  href={link.url}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          
+          
+            
+          </ul>
+          <div className=" block  float-right md:hidden">
+            <Bars3BottomLeftIcon
+              onClick={() => setToggel(!toggel)}
+              className={`w-8 h-8 ${toggel ? "hidden" : "block"}`}
+            />
+           
+          </div>
+          <div  className={`  fixed z-10 top-0 left-0 bg-gradient-to-l from-black  to-gray-800 h-screen   cursor-pointer w-full   ${
+              toggel ? "" : "hidden"
+            }   `}>
+          <XMarkIcon
+              onClick={() => setToggel(!toggel)}
+              className={`w-8 h-8 ${!toggel ? "hidden" : "block"} float-right m-10`}
+            />
+          <ul className="flex flex-col  justify-center h-[80vh] text-center" >
+            {links.map((link,index) => (
+              <motion.li
+              initial={{opacity:0,x:200}}
+  transition={{duration:0.5,delay:0.2*index}}
+  whileInView={{opacity:1,x:0}}
+                className={` flex gap-5  w-fit m-auto text-stone-50 ${(activeItem===link.name?"text-violet-700 ":"")}`}
+                onClick={() => {setToggel(!toggel)
+                  setActiveItem(link.name)
+                }}
+                key={link.name}
+              >
+                {link.icon}
+                <Link
+                  className={`hover:text-gray-200 hover:bg-slate-600 font-serif font-bold text-lg `}
+                  href={link.url}
+                >
+                  {link.name}
+                </Link>
+              </motion.li>
+           
+           ))}
+          </ul>
+         
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
